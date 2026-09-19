f24196ad72e4cef97bf06b5624a22ee08e4afce6
LAKLY <locip477@gmail.com>
2026-09-19 12:10:37 +0900

    Delete .github/workflows directory

--- .github/workflows/electron/preload.js
+++
@@ -1,9 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('lakly', {
-  getSettings: () => ipcRenderer.invoke('settings:get'),
-  setSettings: (data) => ipcRenderer.invoke('settings:set', data),
-  installPluginZip: (zipPath) => ipcRenderer.invoke('plugin:install-zip', zipPath),
-  onTrayCloseRoom: (cb) => ipcRenderer.on('tray:close-room', cb),
-  onTrayCreateRoom: (cb) => ipcRenderer.on('tray:create-room', cb),
-});
